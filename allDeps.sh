dc75c63aa039199f528f4e7935bf2edf31ce5c6b
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2025-12-14 03:44:10 -0300

    Enhance allDeps.sh with permission prompts and installs
    Updated the script to include permission prompts and installation commands for various packages.

--- allDeps.sh
+++ allDeps.sh
@@ -1,1 +1,13 @@
-#Dependencias para compilar, incluido sqlite3 en NodeJS npm
+#!/data/data/com.termux/files/usr/bin/bash
+echo "\e[1;33m⚠️ ACEPTA LOS PERMISOS CUANDO APAREZCAN \e[0m"
+echo "\e[1;33m⚠️ CONCEDE PERMISOS DE ALMACENAMIENTO Y EJECUCION \e[0m"
+sleep 5
+printf 'n\n' | termux-setup-storage
+sleep 7
+termux-wake-lock
+apt-get update
+pkg i -y tur-repo x11-repo
+apt update -y && yes | apt upgrade && pkg install -y android-tools ano clang make git ffmpeg nodejs-lts pkg-config libxml2 libxslt matplotlib xorgproto rust binutils wget build-essential libvips python-pip glib termux-services termux-x11-nightly
+pip install cython wheel setuptools python-dotenv
+
+echo "\e[1;33m Finalizado \e[0m"
