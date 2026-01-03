65302418d9fefa8c0729c919c91f16cfe1c69017
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2026-01-03 16:50:35 -0300

    Update zsh.sh

--- Scripts/AutoScripts/zsh.sh
+++ Scripts/AutoScripts/zsh.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg i -y tur-repo x11-repo
 apt update -y&&yes|apt upgrade
-pkg i -y zsh
+pkg i -y zsh git
 D1="$HOME/.config/pip";F1="$D1/pip.conf"
 mkdir -p "$D1"
 grep -q termux-user-repository "$F1" 2>/dev/null||cat>>"$F1"<<'EOF'
